891b9e7885257739e617fa93e3c7f7717b96865d
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-05-21 21:33:31 +0300

    create ItemList component with module.css

---
+++ src/ItemList/ItemList.js
@@ -0,0 +1,28 @@
+import styles from "./ItemList.module.css"
+import React from "react";
+
+const ItemList = (props) => {
+    const {selectedObject, renderItem, data} = props;
+    const objects = data.map((item) => {
+        const {id} = item;
+        const value = renderItem(item);
+        return (
+            <li key={id}
+                onClick={() => selectedObject(id)}
+                className={styles.Item}>
+                {value}
+            </li>
+        );
+    });
+    return (
+        <div className={styles.ItemList}>
+            <ul>
+                {objects}
+            </ul>
+        </div>
+    );
+
+}
+export default ItemList
+
+
