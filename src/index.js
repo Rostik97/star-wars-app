fc1c8766db6c39fd55884854ac1b149e7ece4c12
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-06-25 22:03:32 +0300

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App/App";
-import {BrowserRouter} from "react-router-dom";
-
+import {HashRouter} from "react-router-dom";
 
 ReactDom.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>, document.getElementById('root'));
