0b04940c64dab167143fe6a047717b9d8b0f5366
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-06-25 21:35:42 +0300

    Updates

---
+++ precache-manifest.7d6e394e4b9df0167378488b655abcae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5247a5769cd7d6a6bd",
+    "url": "/star-wars-app/static/css/main.f66c0c52.chunk.css"
+  },
+  {
+    "revision": "2e5247a5769cd7d6a6bd",
+    "url": "/star-wars-app/static/js/main.b817f78f.chunk.js"
+  },
+  {
+    "revision": "6c384fc4cd79414e663f",
+    "url": "/star-wars-app/static/js/runtime~main.5791dfc2.js"
+  },
+  {
+    "revision": "c1cde235472f8979475d",
+    "url": "/star-wars-app/static/js/2.57e676be.chunk.js"
+  },
+  {
+    "revision": "dbfd1d14508f9975e7cfa796946fe91e",
+    "url": "/star-wars-app/static/media/img.dbfd1d14.png"
+  },
+  {
+    "revision": "711fd082df1553119e7d1caa60228b39",
+    "url": "/star-wars-app/static/media/stub.711fd082.png"
+  },
+  {
+    "revision": "40eb61e73655c8f3658e991514584dd5",
+    "url": "/star-wars-app/static/media/robots.40eb61e7.gif"
+  },
+  {
+    "revision": "87647a3b1579f197652a40db2fe1832d",
+    "url": "/star-wars-app/static/media/clones.87647a3b.gif"
+  },
+  {
+    "revision": "d07a38062d3e4e8d987281c62a5a280b",
+    "url": "/star-wars-app/index.html"
+  }
+];
