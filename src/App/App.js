2d63217857c8a806fa74513d7fd4c2fc357bac74
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-06-25 21:23:53 +0300

    rewrite App with routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,58 +1,41 @@
-import {Component} from "react";
+import React, {Component} from 'react';
 import styles from "./App.module.css"
 import Header from "../Header/Header";
-import RandomPlanet from "../RandomPlanet/RandomPlanet";
-import PersonDetails from "../PersonDetails/PersonDetails";
 import SwapiService from "../Services/SwapiService";
 import ErrorBoundary from "../ErrorBoudry/ErrorBoundary";
-import {PersonList} from "../SwComponents/GeneralItemList";
 import {SwapiServiceProvider} from "../ServiceContext/ServiceContext";
-
+import {Route, Routes} from "react-router-dom";
+import PersonPage from "../Characters/PersonPage/PersonPage";
+import HomePage from "../HomePage/HomePage";
+import PlanetPage from "../Planets/PlanetPage/PlanetPage";
+import StarshipPage from "../Starships/StarshipPage/StarshipPage";
+import PlanetDetails from "../Planets/PlanetDetails/PlanetDetails";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import PersonDetails from "../Characters/PersonDetails/PersonDetails";
+import StarshipDetails from "../Starships/StarshipDetails/StartshipDetails";
 
 export default class App extends Component {
 
     swapiService = new SwapiService()
 
-    state = {
-        personId: 1,
-        startShipId: 1,
-        planetId: 1
-    }
-
-    selectPerson = (id) => {
-        console.log("selectPerson " + id)
-        this.setState({personId: id})
-    }
-
-    selectStarship = (id) => {
-        console.log("selectStarship " + id)
-        this.setState({startShipId: id})
-    }
-
-    selectPlanet = (id) => {
-        console.log("selectPlanet " + id)
-        this.setState({planetId: id})
-    }
-
 
     render() {
-        const {personId, startShipId} = this.state
-        const {getPerson, getPersonImg} = this.swapiService;
-
         return (
             <ErrorBoundary>
                 <SwapiServiceProvider value={this.swapiService}>
                     <div className={styles.App}>
                         <div className={styles.Content}>
                             <Header/>
-                            <RandomPlanet/>
-                            <div className={styles.Row}>
-                                <PersonList selectedObject={this.selectPerson}
-                                            renderItem={(item) => item.name}/>
-                                <PersonDetails personId={personId}
-                                               getPerson={getPerson}
-                                               getPersonImg={getPersonImg}/>
-                            </div>
+                            <Routes>
+                                <Route path="/" element={<HomePage/>}/>
+                                <Route path="/people" element={<PersonPage/>}/>
+                                <Route path="/people/:id" element={<PersonDetails/>}/>
+                                <Route path="/planets" element={<PlanetPage/>}/>
+                                <Route path="/planets/:id" element={<PlanetDetails/>}/>
+                                <Route path="/starships" element={<StarshipPage/>}/>
+                                <Route path="/starships/:id" element={<StarshipDetails/>}/>
+                                <Route path="*" element={<NotFoundPage/>}/>
+                            </Routes>
                         </div>
                     </div>
                 </SwapiServiceProvider>
